ff404e786d44a140921a1a3c335f6ef8d1e25157
Captain Fantasy <captain@fantasyui.com>
2017-07-28 10:47:15 -0400

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 
 
-npm i larceny -G
-larceny -u https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css -s .btn -t .my-button -f style.css
+npx larceny -ca -u https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css -s .btn -t .my-button -f my-style.css
+cat my-style.css
